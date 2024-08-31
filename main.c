685edfd5621ffa6b4e54c313ff6cb287a5933a04
AmrKhaledelsharkawy <amrk13415@gmail.com>
2024-08-31 03:14:05 +0300

    init

--- main.c
+++ main.c
@@ -142,7 +142,7 @@ void End_program(CPU *cpu) {
     // Print out the values of all non-zero general-purpose registers
     fprintf(output_file, "\nRegisters:\n");
     for (int i = 0; i < 64; i++) {
-        if (reg_used[i] != 0) {
+        if (cpu->registers[i] != 0 && i!=64 && i!=65) {
             fprintf(output_file, "R%d: %d\n", i, cpu->registers[i]);  // Print as signed integer
         }
     }
